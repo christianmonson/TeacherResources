2c5de23e1b04ab45f6a8ba7e12f85cb8610966b0
prush08 <parkerrushton@gmail.com>
2015-03-06 13:37:18 -0700

    Initial Commit

---
+++ Teacher Resources/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Teacher Resources
+//
+//  Created by Parker Rushton on 3/6/15.
+//  Copyright (c) 2015 PJayRushton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Teacher Resources/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Teacher Resources
+//
+//  Created by Parker Rushton on 3/6/15.
+//  Copyright (c) 2015 PJayRushton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
